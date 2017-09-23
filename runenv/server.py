b7421c5b304b9597b4dc455361b8c45eb28d3794
syllogismos <anilkaraka@live.in>
2017-09-24 01:54:01 +0530

    fix server bugs

--- runenv/server.py
+++ runenv/server.py
@@ -27,7 +27,8 @@ def dump_episodes(env_name, difficulty,
     # scaler = pickle.load(open(scaler_file, 'rb'))
     p = multiprocessing.Pool(cores, maxtasksperchild=1)
     paths = p.map(get_paths_from_latest_policy,
-            [(env_name, chk_dir, batch_size//cores)]*cores)
+            [(env_name, difficulty, 
+              chk_dir, batch_size//cores)]*cores)
     p.close()
     p.join()
     paths = sum(paths, [])
@@ -39,13 +40,14 @@ def get_paths_from_latest_policy(pickled_obj):
     pickled_obj = (env_name, chk_dir, batch_size_per_core)
     """
     env_name = pickled_obj[0]
-    chk_dir = pickled_obj[1]
-    batch_size_per_core = pickled_obj[2]
+    difficulty = pickled_obj[1]
+    chk_dir = pickled_obj[2]
+    batch_size_per_core = pickled_obj[3]
     with tf.Session() as sess:
         data = joblib.load(chk_dir + 'params.pkl')
         policy = data['policy']
-        env = TfEnv(GymEnv(env_name, difficulty=kwargs['difficulty'],
-            runenv_seed=kwargs['runenv_seed'], visualize=kwargs['visualize'],
+        env = TfEnv(GymEnv(env_name, difficulty=difficulty,
+            runenv_seed=1, visualize=False,
             record_log=False, record_video=False))
         total_length = 0
         paths = []
